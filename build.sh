b239347c949d25ef610676b13d820a7370abee9a
Dopple503 <madaforegon@gmail.com>
2025-06-01 03:20:44 -0700

    Update build script with additional dependencies for dlib

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-apt-get update && apt-get install -y cmake
+apt-get update && apt-get install -y \
+    cmake \
+    build-essential \
+    pkg-config \
+    libx11-dev \
+    libatlas-base-dev \
+    libgtk-3-dev \
+    libboost-python-dev \
+    python3-dev \
+    python3-pip \
+    python3-numpy \
+    python3-scipy \
+    python3-pillow \
+    python3-dlib
+
 pip install --no-cache-dir -r requirements.txt 
