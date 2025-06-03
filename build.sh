c88435aae5433c2d71fed95f7b6da16bac7cd72e
Dopple503 <madaforegon@gmail.com>
2025-06-02 22:33:01 -0700

    Optimize build process with pre-built dlib wheel

--- build.sh
+++ build.sh
@@ -21,8 +21,13 @@ python3 -m venv /opt/venv
 
 # Install Python dependencies
 pip install --upgrade pip
-pip install dlib==19.22.1
-pip install -r requirements.txt
+pip install --no-cache-dir wheel setuptools
+
+# Install dlib from pre-built wheel for Python 3.10
+pip install --no-cache-dir https://github.com/jloh02/dlib/releases/download/v19.22/dlib-19.22.1-cp310-cp310-linux_x86_64.whl
+
+# Install other dependencies
+pip install --no-cache-dir -r requirements.txt
 
 # Make sure the virtual environment is used when running the app
 echo "source /opt/venv/bin/activate" >> /etc/profile.d/venv.sh 
